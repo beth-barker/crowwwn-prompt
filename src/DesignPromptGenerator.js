bb8b8ddc437717243cf0c7abfb34c670c5b1374a
Beth Barker <barker.k.elizabeth@gmail.com>
2023-10-06 10:23:04 -0600

    css fixed

--- src/DesignPromptGenerator.js
+++ src/DesignPromptGenerator.js
@@ -1,18 +1,25 @@
-import React, { useState } from "react";
+import React, { useState} from "react";
 import {CopyToClipboard} from "react-copy-to-clipboard"
 import axios from "axios";
 import Logo from './crowwwnLogo.png'
 import './DesignPrompt.css'
 import Modal from './Modal'
+import {Button} from "rsuite";
+import "rsuite/dist/rsuite-no-reset.min.css"; 
 
 const API_KEY = process.env.REACT_APP_API_KEY;
 
 const DesignPromptGenerator = () => {
+  const ButtonStyle = {backgroundColor: "#3F66EF", margin: "0px 0px 10px 0px" };
+
   const [prompt, setPrompt] = useState("");
   const [copy, setCopy] = useState("Copy")
   const [copied, setCopied] = useState(false)
+  const [show, setShow] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   const generatePrompt = async () => {
+    setLoading(true);
     try {
       const response = await axios.post(
         "https://api.openai.com/v1/chat/completions",
@@ -36,6 +43,7 @@ const DesignPromptGenerator = () => {
 
       if(generatedPrompt.endsWith('.')){
         setPrompt(generatedPrompt)
+        setLoading(false);
       } else {
         generatePrompt();
       }
@@ -55,26 +63,26 @@ const DesignPromptGenerator = () => {
     if(prompt.length > 1) {
       setCopy('Copied')
       setCopied(true)
+      setShow(true)
+      setTimeout(() => {
+        setShow(false)
+      }, 900)
     }
   }
-  
- 
 
   return (
     <div className="prompt">
         <img className="logo" src={Logo} alt="crowwwnLogo" />
-        <h1>
-          {prompt.length > 1 ? prompt : 'Use A.I. to generate prompts for your next UX Design project' }
-        </h1>
+        {prompt.length > 1 ? <h2 className="prompt-text">"{prompt}"</h2> : <h1>Use A.I. to generate prompts for your next UX Design project</h1>}
         <div className="btns">
-          <button className="prompt-btn"onClick={generatePrompt}>Generate Prompt</button>
+          {loading ? <Button loading appearance="primary" style={ButtonStyle}className="prompt-btn">Loading...</Button> : <button className="prompt-btn"onClick={generatePrompt}>Generate Prompt</button>}
           <CopyToClipboard text={prompt}>
             <button className="copy-btn" onClick={showAlert}>{copy}</button>
           </CopyToClipboard>
         </div>
         <p>Thank you for supporting Crowwwn</p>
         <div className="modal">
-          {copied ? <Modal/> : null}
+          {copied && show ? <Modal/> : null}
         </div>
     </div>
   );
